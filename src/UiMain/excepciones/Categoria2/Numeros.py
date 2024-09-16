91ca716854ed4f5dc773803b047ddec7052f1a00
ValentinaLujan12 <vlujan@unal.edu.co>
2024-09-16 14:40:33 -0500

    Excepcion

--- src/UiMain/excepciones/Categoria2/Numeros.py
+++
@@ -1,5 +0,0 @@
-from UiMain.excepciones.Categoria2.ExceptionC2 import ExceptionC2
-
-class Numeros(ExceptionC2):
-    def __init__(self):
-        super().__init__("No se pueden poner caracteres que no son numeros en los campos de cantidad")
