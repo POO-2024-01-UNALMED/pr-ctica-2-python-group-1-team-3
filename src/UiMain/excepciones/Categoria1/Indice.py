058a8c01e32416088da15dbd55427ef2db342019
ValentinaLujan12 <vlujan@unal.edu.co>
2024-09-16 10:04:38 -0500

    Excepcion incorrecta

--- src/UiMain/excepciones/Categoria1/Indice.py
+++
@@ -1,5 +0,0 @@
-from UiMain.excepciones.Categoria1.ExceptionC1 import ExceptionC1
-
-class Indice(ExceptionC1):
-    def __init__(self):
-        super().__init__("Seleccione un producto en todas las casillas disponibles")
